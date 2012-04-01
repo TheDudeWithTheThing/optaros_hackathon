71016bda93949e3d8e96d62bfd1d55dbf6e208fe
Jeff Green <jeff.david.green@gmail.com>
2012-04-01 13:56:07 -0400

    Finally got the thing to draw events!

--- frontend/script.js
+++ frontend/script.js
@@ -2,26 +2,22 @@ var canvas = document.getElementById('calendar');
 var eventBoxWidth = 260;
 var eventLeftOffset = 50;
 
-var data = {"_id":"4f785ba0ac6ca42d7d57080e","room":"boston-downtown@optaros.com","CalendarEvent":[{"BusyType":"Busy","EndTime":"2012-04-02T07:30:00","StartTime":"2012-04-02T07:00:00"},{"BusyType":"Busy","EndTime":"2012-04-02T09:00:00","StartTime":"2012-04-02T08:30:00"},{"BusyType":"Busy","EndTime":"2012-04-02T11:30:00","StartTime":"2012-04-02T10:30:00"},{"BusyType":"Busy","EndTime":"2012-04-09T10:00:00","StartTime":"2012-04-09T09:00:00"},{"BusyType":"Busy","EndTime":"2012-04-16T10:00:00","StartTime":"2012-04-16T09:00:00"},{"BusyType":"Busy","EndTime":"2012-04-23T10:00:00","StartTime":"2012-04-23T09:00:00"},{"BusyType":"Busy","EndTime":"2012-04-26T09:00:00","StartTime":"2012-04-26T08:00:00"},{"BusyType":"Busy","EndTime":"2012-04-30T10:00:00","StartTime":"2012-04-30T09:00:00"}]};
+var data = {"_id":"4f785ba0ac6ca42d7d57080e","room":"boston-downtown@optaros.com","CalendarEvent":[{"BusyType":"Busy","EndTime":"2012-04-02T07:30:00","StartTime":"2012/04/02T07:00:00"},{"BusyType":"Busy","EndTime":"2012-04-02T13:40:00","StartTime":"2012/04/02T08:30:00"},/* {"BusyType":"Busy","EndTime":"2012-04-02T11:30:00","StartTime":"2012-04-02T10:30:00"},{"BusyType":"Busy","EndTime":"2012-04-09T10:00:00","StartTime":"2012-04-09T09:00:00"},{"BusyType":"Busy","EndTime":"2012-04-16T10:00:00","StartTime":"2012-04-16T09:00:00"},{"BusyType":"Busy","EndTime":"2012-04-23T10:00:00","StartTime":"2012-04-23T09:00:00"},{"BusyType":"Busy","EndTime":"2012-04-26T09:00:00","StartTime":"2012-04-26T08:00:00"},{"BusyType":"Busy","EndTime":"2012-04-30T10:00:00","StartTime":"2012-04-30T09:00:00"} */]};
 
 startTime();
 setDate();
 
-
 // Check the element is in the DOM and the browser supports canvas
 if (canvas.getContext) {
-    drawTimeLines();
-    drawEvent(495, 540);
-    
-    alert(timeInMinutes());
-    for (var info in data.CalendarEvent) {
-/*         var start = timeInMinutes(info.StartTime); */
-/*         drawEvent(info.StartTime, info.EndTime, info.BusyType); */
+    var context = canvas.getContext('2d');
+    drawTimeLines(context);
+    for (var i = 0; i < data.CalendarEvent.length;  i++) {
+         var info = data.CalendarEvent[i]
+         drawEvent(context, timeInMinutes(hackDate(info.StartTime)), timeInMinutes(hackDate(info.EndTime)));
     }
 }
 
-function drawTimeLines() {
-    var context = canvas.getContext('2d');
+function drawTimeLines(context) {
     for (h = 0; h < 24; h++) {
         if (h==0) continue;
         beginY = h * 60 * 2;
@@ -50,10 +46,9 @@ function drawTimeLines() {
 }
 
 // startTime and endTime must be in minutes starting from midnight.
-function drawEvent (startTime, endTime, available) {
+function drawEvent (context, startTime, endTime) {
     var begin = startTime * 2;
     var duration = (endTime - startTime) * 2;
-    var context = canvas.getContext('2d');
     context.fillRect(eventLeftOffset, begin, eventBoxWidth, duration);   
 }
 
@@ -110,5 +105,10 @@ function timeInMinutes(time) {
     var date = new Date(time);
     var m = date.getMinutes();
     var h = date.getHours() * 60;
-    return m + h;
+    var total = m + h;
+    return total;
+}
+
+function hackDate(d) {
+    return d.replace(/-/g, '/').replace('T', ' ');
 }
